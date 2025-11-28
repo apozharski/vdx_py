308495998840ff537350e9f750a381811fdbc053
Anton Pozharskiy <apozharski@gmail.com>
2025-11-28 18:39:46 +0100

    minor bugfixes

--- src/vdx_py/nlp.py
+++ src/vdx_py/nlp.py
@@ -3,6 +3,7 @@ from .vector import *
 
 class NLP:
     def __init__(self,symbolic_type=ca.SX):
+        self.obj = symbolic_type(0)
         self.w = PrimalVector(symbolic_type=symbolic_type)
         self.g = ConstraintVector(symbolic_type=symbolic_type)
-        self.p = ConstraintVector(symbolic_type=symbolic_type)
+        self.p = ParameterVector(symbolic_type=symbolic_type)
