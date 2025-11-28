bace95a2d3506813a6f9914715421513b87fe317
Anton Pozharskiy <apozharski@gmail.com>
2025-11-28 08:48:33 +0100

    nlp building is now fully working

--- src/vdx_py/nlp.py
+++ src/vdx_py/nlp.py
@@ -1,5 +1,8 @@
 import casadi as ca
+from .vector import *
 
 class NLP:
-    def __init__(symbolic_type=ca.SX):
-        self.w = symbolic_type()
+    def __init__(self,symbolic_type=ca.SX):
+        self.w = PrimalVector(symbolic_type=symbolic_type)
+        self.g = ConstraintVector(symbolic_type=symbolic_type)
+        self.p = ConstraintVector(symbolic_type=symbolic_type)
--- src/vdx_py/vector.py
+++ src/vdx_py/vector.py
@@ -8,10 +8,10 @@ from typing import Any
 class Primal:
     name : str
     size : int = 1
-    lb = -np.inf
-    ub = np.inf
-    init = 0.0
-    init_mult = 0.0
+    lb : Any = -np.inf
+    ub : Any = np.inf
+    init : Any = 0.0
+    init_mult : Any = 0.0
 
     def __post_init__(self):
         self.lb = self.lb*np.ones(self.size)
@@ -24,10 +24,10 @@ class Primal:
 
 @dataclass
 class Constraint:
-    sym       = ca.SX([])
-    lb        = -np.inf
-    ub        = np.inf
-    init_mult = 0.0
+    sym : Any
+    lb : Any = -np.inf
+    ub : Any = np.inf
+    init_mult : Any = 0.0
 
     def __post_init__(self):
         size = self.sym.size()[0]
@@ -35,13 +35,19 @@ class Constraint:
         self.ub = self.ub*np.ones(size)
         self.init_mult = self.init_mult*np.ones(size)
 
+    def __len__(self):
+        return self.sym.size()[0]
+
 @dataclass
 class Parameter:
     name : str
     size : int = 1
-    val = 0.0
+    val : Any = 0.0
     def __post_init__(self):
         self.val = self.val*np.ones(self.size)
+
+    def __len__(self):
+        return self.size
 
 class Vector:
     def __init__(self, symbolic_type=ca.SX):
@@ -91,17 +97,41 @@ class PrimalVector(Vector):
 
 
 class ConstraintVector(Vector):
-    def __init__(symbolic_type=ca.SX):
+    def __init__(self, symbolic_type=ca.SX):
         super().__init__(symbolic_type=symbolic_type)
         self.lb = np.array([])
         self.ub = np.array([])
         self.init_mult = np.array([])
 
         # Results
-        self.res = np.array([])
         self.mult = np.array([])
 
+    def add_var(self, value):
+        if not isinstance(value, Constraint):
+            ValueError("You did not pass a Constraint object to this initializer")
+        # TODO(@anton) implement the caching that vdx does in matlab
+
+        self.sym = ca.vertcat(self.sym, value.sym)
+        self.lb = np.append(self.lb, value.lb)
+        self.ub = np.append(self.ub, value.ub)
+        self.init_mult = np.append(self.init_mult, value.init_mult)
+        self.mult = np.append(self.mult, value.init_mult)
+        indices = range(self.nelem, self.nelem + len(value))
+        self.nelem += len(value)
+        return indices
+
 class ParameterVector(Vector):
-    def __init__(symbolic_type=ca.SX):
+    def __init__(self, symbolic_type=ca.SX):
         super().__init__(symbolic_type=symbolic_type)
         self.val = np.array([])
+
+    def add_var(self, value):
+        if not isinstance(value, Parameter):
+            ValueError("You did not pass a Parameter object to this initializer")
+        # TODO(@anton) implement the caching that vdx does in matlab
+
+        self.sym = ca.vertcat(self.sym, self.new_sym(value.name, value.size))
+        self.val = np.append(self.val, value.val)
+        indices = range(self.nelem, self.nelem + len(value))
+        self.nelem += len(value)
+        return indices
